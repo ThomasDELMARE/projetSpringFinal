b5ceae8ad98417985d85b3cab8d1bc26b874a942
Thomas DELMARE <delmarethomas1@gmail.com>
2023-01-31 16:18:19 +0100

    Enable Eureka client et dashboard dans MyFinder

--- myFinder/src/main/java/com/example/myfinder/MyFinderApplication.java
+++ myFinder/src/main/java/com/example/myfinder/MyFinderApplication.java
@@ -3,8 +3,10 @@ package com.example.myfinder;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @EnableCircuitBreaker
+@EnableEurekaClient
 @SpringBootApplication
 public class MyFinderApplication {
 
