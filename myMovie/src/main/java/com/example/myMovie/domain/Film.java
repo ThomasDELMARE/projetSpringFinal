50f1a9eb788dbe2a5caf606b15c36e7872363898
Thomas DELMARE <delmarethomas1@gmail.com>
2023-01-31 11:38:03 +0100

    Création des microservices et ajout de la classe Film

---
+++ myMovie/src/main/java/com/example/myMovie/domain/Film.java
@@ -0,0 +1,49 @@
+package com.example.myMovie.domain;
+
+import java.util.Date;
+
+public class Film {
+    // On crée le film avec les propriétés
+    String titre, realisateur, acteurPrincipal;
+    Date dateDeSortie;
+
+    public Film(String titre, String realisateur, String acteurPrincipal, Date dateDeSortie){
+        super();
+        this.titre = titre;
+        this.realisateur = realisateur;
+        this.acteurPrincipal = acteurPrincipal;
+        this.dateDeSortie = dateDeSortie;
+    }
+
+    public String getTitre() {
+        return titre;
+    }
+
+    public void setTitre(String titre) {
+        this.titre = titre;
+    }
+
+    public String getRealisateur() {
+        return realisateur;
+    }
+
+    public void setRealisateur(String realisateur) {
+        this.realisateur = realisateur;
+    }
+
+    public String getActeurPrincipal() {
+        return acteurPrincipal;
+    }
+
+    public void setActeurPrincipal(String acteurPrincipal) {
+        this.acteurPrincipal = acteurPrincipal;
+    }
+
+    public Date getDateDeSortie() {
+        return dateDeSortie;
+    }
+
+    public void setDateDeSortie(Date dateDeSortie) {
+        this.dateDeSortie = dateDeSortie;
+    }
+}
